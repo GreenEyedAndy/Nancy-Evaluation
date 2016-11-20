780e31c867c7658022b6c23bee5f227ba1a7c365
Andreas Patock <apatock@googlemail.com>
2016-11-20 16:12:35 +0100

    Mit Webseite

---
+++ NancyEvaluation/NancyEvaluation/model/WelcomeModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Timers;
+
+namespace NancyEvaluation.model
+{
+    public class WelcomeModel
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ NancyEvaluation/NancyEvaluation/modules/RootModule.cs
@@ -0,0 +1,14 @@
+﻿using Nancy;
+using NancyEvaluation.model;
+
+namespace NancyEvaluation.Modules
+{
+    public class RootModule : NancyModule
+    {
+        public RootModule()
+        {
+            var model = new WelcomeModel { Name = "Andy" };
+            Get["/"] = parameters => View["index.html", model];
+        }
+    }
+}
