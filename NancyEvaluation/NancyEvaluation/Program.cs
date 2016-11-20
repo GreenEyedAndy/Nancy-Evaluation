94a7a09a4dea125cef96e69c9a1eaf66e23254cf
Andreas Patock <apatock@googlemail.com>
2016-11-20 15:52:44 +0100

    Erster Test mit Nancy
    Aus der dotnetpro 11/16

---
+++ NancyEvaluation/NancyEvaluation/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nancy.Hosting.Self;
+
+namespace NancyEvaluation
+{
+    class Program
+    {
+        const string url = "http://localhost:8080";
+        static void Main(string[] args)
+        {
+            var hostConfiguration = new HostConfiguration();
+            hostConfiguration.UrlReservations.CreateAutomatically = true;
+
+            using (var host = new NancyHost(hostConfiguration, new Uri(url)))
+            {
+                host.Start();
+                Console.WriteLine($"Running on {url}");
+                Console.ReadLine();
+            }
+        }
+    }
+}
